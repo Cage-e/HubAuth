d8773625af317548329be66e844cf2c2b88f57ea
Kian G <cage.e@hotmail.com>
2022-08-07 17:13:31 +0200

    Add project files.

---
+++ HubAuth/Client/IncludeRequestCredentialsMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+namespace HubAuth.Client
+{
+    public class IncludeRequestCredentialsMessageHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ HubAuth/Server/Hub1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HubAuth.Server
+{
+    [Authorize]
+    public class Hub1 : Hub
+    {
+        public async Task SendMessage(int clientUpdate) => await Clients.All.SendAsync("ReceiveMessage", clientUpdate + 5);
+
+    }
+}
